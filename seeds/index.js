96760acf7151c6db9e7bc2cd8ba3f25054d405a0
Aditya singh <aditya.rpsk@gmail.com>
2024-12-27 15:21:45 +0530

    added bootstrap and some style to show

--- seeds/index.js
+++ seeds/index.js
@@ -19,9 +19,13 @@ const sample = array=> array[Math.floor(Math.random() * array.length)];
     for(let i =0;i<50;i++)
     {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random()*20)+10;
         const camp=new Campground({
             location:`${cities[random1000].city},${cities[random1000].state}`,
-            title:`${sample(descriptors)} ${sample(places)}`
+            title:`${sample(descriptors)} ${sample(places)}`,
+            image:`https://picsum.photos/600?random=${Math.random()}`,
+            description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati adipisci nihil voluptatibus magni quisquam provident, placeat quo reprehenderit ad atque eaque, sapiente ex. Recusandae cumque dolorum quis saepe assumenda veritatis?',
+            price
         })
 
         await camp.save();
