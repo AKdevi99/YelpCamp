43da286835fa22b399cb0d273c57d3f66ca45f56
Aditya singh <aditya.rpsk@gmail.com>
2025-01-08 20:27:37 +0530

    added a error template

--- app.js
+++ app.js
@@ -93,8 +93,9 @@ app.all('*',(req,res,next)=>{
 
 //adding a error handler
 app.use((err,req,res,next)=>{
-    const {statusCode=500,message = " something went wrong"} = err;
-    res.status(statusCode).send(message);
+    const {statusCode=500} = err;
+    if(!err.message) err.message = "oh no!,Something went wrong";
+    res.status(statusCode).render('error',{err});
     
 })
 
