4141e67784b05d88458fcd1489a44ed26d88e8a1
Aditya singh <aditya.rpsk@gmail.com>
2025-02-11 10:17:07 +0530

    set up passport

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const ejsmate = require('ejs-mate');
 const joi = require('joi');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+
 
 
 
@@ -47,6 +51,13 @@ const sessionConfig = {
 }
 app.use(session(sessionConfig));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());//this should be used after sessions()
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.use((req, res, next) => {
     const successMessages = req.flash("success");
---
+++ models/user.js
@@ -0,0 +1,19 @@
+
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const Schema = mongoose.Schema;
+
+
+const userSchema = new Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    }
+});
+
+
+userSchema.plugin(passportLocalMongoose);//add all the featurs of authentification
+
+module.exports = mongoose.model('User',userSchema);
